4007212ab0ca4d3171a4f64b645d0626a97a4370
Nick Tenebruso <nickten2006@gmail.com>
2022-03-27 14:40:23 -0400

    fix: Change origin to localhost

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://127.0.0.1:3000"],
+        origin: ["http://localhost:3000"],
     },
 });
 
